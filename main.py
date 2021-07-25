2ce437fe22da5729a291036161c0deb38c312da2
sinryan137 <sineadryan137@gmail.com>
2021-07-25 12:14:05 +0100

    Brief view of info within both dataframes

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 EFTs=pd.read_csv("ETFs.csv")
 
 print(EFTs.head())
 
 print(EFTs.info())
+
+print(EFTs.columns)
+
+print(EFTs.index)
+
+print(EFTs.head(5))
+
+MFunds=pd.read_csv("Mutual Funds.csv", low_memory=False)
+
+print(MFunds.head())
+print(MFunds.shape)
+print(MFunds.info)
 
 
 
